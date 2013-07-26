1a1af33f7684366bb09e7d3b12d49fe2833517d3
f-torija <ftorija@mit.edu>
2013-07-26 18:55:20 -0400

    Added code to write fitness cases and target cases to a .csv file

--- fitness_case_generator.py
+++ fitness_case_generator.py
@@ -1,47 +1,45 @@
+import csv
 def fitness_case_gen(fxn, range_limits=10, neg_range=True):
     """Takes a function as a string and creates a number list for the GPsearch"""
     fitness_list = []
     target_list = []
-    range_lim_c = range_limits*100
+    var_list = []
+    var_list.insert(0, 'y')
+    range_lim_c = int((range_limits+.5)*100)
     if not neg_range:
         negs = 0
     else:
         negs = range_lim_c*-1
-  
-    if 'x0' in fxn:
-        for x0 in range(negs, range_lim_c, 50):
-            x0 = float(x0)/100
-            if 'x1' in fxn:
-                for x1 in range(negs, range_lim_c, 50):
-                    x1 = x1/100
-                    list_1 = [x0,x1]
-                    solution = eval(fxn)
-                    fitness_list.append(list_1)
-                    target_list.append(solution)
-                
-            else:
+    var_list.insert(0,'x0')
+    for x0 in range(negs, range_lim_c, 50):
+        x0 = float(x0)/100
+        if 'x1' in fxn:
+            
+            for x1 in range(negs, range_lim_c, 50):
+                x1 = float(x1)/100
+                list_1 = [x0,x1]
                 solution = eval(fxn)
-                list_1 = [x0]
                 fitness_list.append(list_1)
                 target_list.append(solution)
-        print target_list, fitness_list    
-        
-    elif 'x1' in fxn and not 'x0' in fxn:
-        for x1 in range (negs, range_lim_c, 50):
-            x1 = float(x1)/100
-            list_1 = [x0,x1]
+            
+        else:
             solution = eval(fxn)
+            list_1 = [x0]
             fitness_list.append(list_1)
             target_list.append(solution)
-            return target_list, fitness_list
-##For numerical only cases
-##    else:
-##        for _ in range(negs, range_lim_c, 50):
-##            try:
-##                solution = eval(fxn)
-##                fitness_list.append(
-##            except:
-##                pass
+    if 'x1' in fxn:
+        var_list.insert(1,'x1')
+    list_file = open('list_file.csv', 'w+b')
+    writer = csv.writer(list_file)
+    writer.writerow(var_list)
+    print var_list
+    range_of_val = ((range_lim_c-negs)/50)**2
+    
+    for i in range(range_of_val):
+        print fitness_list[i]
+        writer.writerow((fitness_list[i][0], fitness_list[i][1] , target_list[i]))
+    
+    
             
 if __name__ == '__main__':
     fitness_case_gen('x0**2+2*x1', 5, False)
